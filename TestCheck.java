96ebd98039288504f913703586a3ba6a60c40e4b
Silo Lwanda Mweli <165194260+Stylo321@users.noreply.github.com>
2025-04-22 12:02:54 +0200

    Create TestCheck.java
    Signed-off-by: Silo Lwanda Mweli <165194260+Stylo321@users.noreply.github.com>

---
+++ TestCheck.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestCheck {
+    @Test
+    void simpleAddition() {
+        assertEquals(2, 1 + 1);
+    }
+}
